72a8707b998e315d097f488be53f32b170278d30
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-02-28 07:11:50 -0500

    Add very basic form for twitter_accounts#new

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -1,2 +1,9 @@
 class TwitterAccountsController < ApplicationController
+  def index
+    @twitter_accounts = TwitterAccount.all
+  end
+
+  def new
+    @twitter_account = TwitterAccount.new
+  end
 end

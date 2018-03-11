b194c721c68dd5a029eb0ec2e2a5c0155bfd92c8
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-03-11 07:10:22 -0400

    Add infratsucture to create replies

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,10 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.paginate(page: params[:page]).order("tweeted_at DESC")
+    if params[:twitter_account_id]
+      arel = TwitterAccount.find(params[:twitter_account_id]).tweets
+    else
+      arel = Tweet
+    end
+    @tweets = arel.paginate(page: params[:page]).order("tweeted_at DESC")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :twitter_accounts
+  resources :twitter_accounts do
+    resources :tweets, shallow: true, only: :index
+  end
+
+  resources :tweets do
+    resources :replies, only: [:new, :create]
+  end
+
   root "tweets#index"
 end

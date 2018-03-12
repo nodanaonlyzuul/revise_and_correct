d83140feeef108191198f995c37e596afaa16f8e
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-03-12 16:49:23 -0400

    Add uniq constraint on TwitterAccount#screen_name

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,5 +1,6 @@
 class TwitterAccount < ApplicationRecord
   has_many :tweets, dependent: :destroy
   validates_presence_of :screen_name
+  validates_uniqueness_of :screen_name
   validates_presence_of :twitter_id
 end
--- spec/models/twitter_account_spec.rb
+++ spec/models/twitter_account_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TwitterAccount, type: :model do
+  it { is_expected.to validate_uniqueness_of(:screen_name) }
   it { is_expected.to validate_presence_of(:screen_name) }
   it { is_expected.to have_many(:tweets) }
 end

77f5aec100a7f26fc9daeac69a07031f97ea7ce7
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-04-24 08:41:38 -0400

    Create and spec scope of users with Twitter credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :screen_name
   validates_presence_of :email
-  # validates_presence_of :twitter_id
+
+  scope :with_twitter_credentials, -> { where.not(key: nil, secret: nil)}
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe User, type: :model do
   it {  is_expected.to validate_presence_of :name }
   it {  is_expected.to validate_presence_of :screen_name }
   it {  is_expected.to validate_presence_of :email }
+
+  describe 'with_twitter_credentials' do
+    before :each do
+      User.destroy_all
+    end
+
+    it 'Will return 0 for a new user' do
+      create(:user)
+      expect(User.with_twitter_credentials.count).to eq(0)
+    end
+
+    it 'Will return users with Twitter credentials' do
+      user = create(:user, key: 'aqui', secret: 'mrroboto')
+      expect(User.with_twitter_credentials.count).to eq(1)
+      expect(User.with_twitter_credentials.first).to eq(user)
+    end
+  end
 end
